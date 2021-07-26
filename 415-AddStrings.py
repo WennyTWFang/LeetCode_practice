044477013c2ae68e643d2375d631be86bbdf7d71
WennyTWFang <73278045+WennyTWFang@users.noreply.github.com>
2021-07-26 19:27:37 +0800

    Create 415-AddStrings.py

---
+++ 415-AddStrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        try:
+            num1 = int(num1)
+            num2 = int(num2)
+        except:
+            return
+        ans = num1+num2
+        return str(ans)
